46c8eacc58ecad23893bd13d249cc8c40b90313d
mark-fogle <mark.fogle@theactigraph.com>
2020-09-20 15:00:15 -0500

    fix: changed weather topic logging to debug

--- src/consumers/models/weather.py
+++ src/consumers/models/weather.py
@@ -16,7 +16,7 @@ class Weather:
 
     def process_message(self, message):
         """Handles incoming weather data"""
-        logger.info(f"weather event: {message.value()}")
+        logger.debug(f"weather event: {message.value()}")
         weather_data = message.value()
         self.temperature = weather_data['temperature']
         self.status = weather_data['status']
